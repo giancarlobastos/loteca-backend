ba04661bc03f26fa4b272553855422804658b8f9
Giancarlo Bastos Fernandes <giancarlo.bastos.fernandes@gmail.com>
2022-04-30 23:56:58 -0300

    abrreviation

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `team` (
   `id` int PRIMARY KEY,
   `name` varchar(255),
+  `abbreviation` varchar(3),
   `logo` varchar(255),
   `country` varchar(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
